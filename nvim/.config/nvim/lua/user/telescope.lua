935f32eff9efaf51f1a7a4ab523c6a3e3ff9025c
Josh Keller <jrkeller@gmail.com>
2022-08-28 22:31:56 -0400

    Nvim|telescope - add close in normal mode

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -49,6 +49,7 @@ telescope.setup {
 
       n = {
         ["<esc>"] = actions.close,
+        ["<C-c>"] = actions.close,
         ["<CR>"] = actions.select_default,
         ["<C-x>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
