d749c8b99af563beb4f938fa9aa8ff50f3bbdea5
Josh Keller <jrkeller@gmail.com>
2022-09-08 08:31:45 -0400

    Add journal template to orgmode

--- nvim/.config/nvim/lua/user/orgmode.lua
+++ nvim/.config/nvim/lua/user/orgmode.lua
@@ -3,4 +3,11 @@ require('orgmode').setup_ts_grammar()
 require('orgmode').setup({
   org_agenda_files = {'~/notes/org/*'},
   org_default_notes_file = '~/notes/org/refile.org',
+  org_capture_templates = {
+    j = {
+      description = 'Journal',
+      template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
+      target = '~/notes/org/journal.org'
+    }
+  },
 })
