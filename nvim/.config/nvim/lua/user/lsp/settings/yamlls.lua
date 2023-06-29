d2f85edc8c458962eba051c26cbc3e94f356cec2
Josh Keller <jrkeller@gmail.com>
2023-06-28 22:51:06 -0400

    Nvim: Add LS and yaml config

--- nvim/.config/nvim/lua/user/lsp/settings/yamlls.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/yamlls.lua
@@ -1,12 +1,44 @@
-local schemas = {
-  ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"]= "conf/**/*catalog*",
-}
+-- local schemas = {
+--   ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"]= "conf/**/*catalog*",
+-- }
 local opts = {
   settings = {
     yaml = {
-      schemas = schemas,
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
+      customTags = {
+        "!Cidr",
+        "!Cidr sequence",
+        "!And",
+        "!And sequence",
+        "!If",
+        "!If sequence",
+        "!Not",
+        "!Not sequence",
+        "!Equals",
+        "!Equals sequence",
+        "!Or",
+        "!Or sequence",
+        "!FindInMap",
+        "!FindInMap sequence",
+        "!Base64",
+        "!Join",
+        "!Join sequence",
+        "!Ref",
+        "!Sub",
+        "!Sub sequence",
+        "!GetAtt",
+        "!GetAZs",
+        "!ImportValue",
+        "!ImportValue sequence",
+        "!Select",
+        "!Select sequence",
+        "!Split",
+        "!Split sequence",      -- schemas = schemas,
+      },
     },
-  },
+  }
 }
 
+print("Returning Yamlls")
 return opts
