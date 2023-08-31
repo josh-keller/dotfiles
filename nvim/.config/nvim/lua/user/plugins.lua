8e6c49f34d797d0d583613b6597764163d5559bb
Josh Keller <jrkeller@gmail.com>
2023-08-30 23:58:20 -0400

    Trying octo.nvim

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -139,7 +139,17 @@ return packer.startup(function(use)
       'nvim-lua/plenary.nvim'
     },
   }
-
+  use {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function ()
+      require"octo".setup()
+    end
+  }
 
   -- Languages
   use "fatih/vim-go"
